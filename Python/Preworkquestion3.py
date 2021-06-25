f987a89daf3496b5a60eba9dc0c7e2ebec87c02b
LifeTruther <walkerian7@gmail.com>
2021-06-25 17:03:02 -0500

    Trying commit using the interface on VS

---
+++ Python/Preworkquestion3.py
@@ -0,0 +1,7 @@
+#oof
+def max_num_in_list(numbers):
+    numbers.sort()
+    print (numbers[-1])
+testlist=[3,5,7,4]       
+max_num_in_list(testlist)
+#terminal output was 7 for the testlist
--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,2 +1,17 @@
 print("Hello World")
+# I am including my python 102 assignment 3 here as well
+# Question 1:
+def hello_name(user_name):
+        print("hello_"+user_name.upper()+"!")
+hello_name('LifeTruther')
+#Result from terminal: hello_LIFETRUTHER! hmm no quotes
 
+# Question 2:
+start, end = 0,200
+for num in range(start,end):
+    if num % 2 !=0:
+        print(num, end = "")
+# Result from terminal: a list of 100 odd numbers aligned vertically
+#I'm a little intimidated by the immediate complexities that this kind of problem inroduces...
+
+#Question 3:
