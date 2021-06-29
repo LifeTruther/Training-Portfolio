0d38ee468fb202b1e5102aa1e8442fc07eb9175d
LifeTruther <walkerian7@gmail.com>
2021-06-29 14:12:03 -0500

    Think I am staaaarrrrrting to get the hang of this

--- Python/preworkquestion5.py
+++ Python/preworkquestion5.py
@@ -1,9 +1,9 @@
 def is_consecutive(a_list):
     if sorted(a_list) == list(range(min (a_list), max(a_list)+1)):
-        print("Yes!")
+        return True
     else:
-        print("No!")
-#Several questions on this one
-x = [1,2,3,4,5,6,7,8,9]
-is_consecutive(x)
-#the terminal seemed very happy with this one, giving me true Yes and true No on test.
+        return False
+# Several questions on this one
+amr = [1,2,3,4,5,6,7,8,9]
+print(is_consecutive(amr))
+# the terminal seemed very happy with this one, giving me true Yes and true No on test.
