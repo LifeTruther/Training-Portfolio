756b490fad0e7cb4eb44b2b8a549b489419c2edf
LifeTruther <walkerian7@gmail.com>
2021-06-29 22:42:30 -0500

    This should work!

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,3 +1,2 @@
 print("Hello World")
-# I am including my python 102 assignment 3 here as well
 
--- Python/preworkquestion4.py
+++ Python/preworkquestion4.py
@@ -5,10 +5,16 @@ def is_leap_year(a_year):
         if a_year % 100 == 0:
             if a_year % 400 ==0:
                 print("That is a leap year!")
-    elif a_year % 4 == 0:
-        if a_year % 100 != 0:
-            print("That is a leap year!")            
+            else:
+                print ("I'm sorry, that is not a leap year")
+        else:
+             print ("That is a leap year!")
+  #  elif a_year % 4 == 0:
+
+# 
+   #     if a_year % 100 != 0:
+  #          print("That is a leap year!")            
     else: 
         print("I'm sorry, that is not a leap year")
-is_leap_year(1600)
+is_leap_year(1900)
 #terminal was a bit buggy with this one: it returned the strings for the right years, but it would magically stop printing the message for the leap year after a few iterations.
